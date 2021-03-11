a4b6b44e01ebde7addacf38e85ec0036b23f72b7
fauziru <fauzirezaumr@gmail.com>
2021-03-11 14:40:14 +0700

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { createLogger } from "vuex";
+import layout from "./layout";
 
 Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== "production";
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {
+    layout
+  },
+  strict: debug,
+  plugins: debug ? [createLogger()] : []
 });
